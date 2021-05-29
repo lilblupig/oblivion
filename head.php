fe89304642e4ac2ed83f005e882ea37b71590ce8
lilblupig <lilblupig@gmail.com>
2021-05-29 20:39:04 +0100

    Write head info into common file

---
+++ head.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Oblivion, Elder Scrolls, RPG, Open world, Quest tracker">
+    <meta name="description" content="Progress tracker for the Bethesda game Oblivion">
+    <link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Oblivion Tracker</title>
+</head>
