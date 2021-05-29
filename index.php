a64842b2929b114960ed488e368c5a0ce9069750
lilblupig <lilblupig@gmail.com>
2021-05-29 19:41:25 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en-uk">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="Oblivion, Elder Scrolls, RPG, Open world, Quest tracker">
+        <meta name="description" content="Progress tracker for the Bethesda game Oblivion">
+        <link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Oblivion Tracker</title>
+    </head>
+
+    <body>
+    <p>Is dis work</p>
+    </body>
+</html>
