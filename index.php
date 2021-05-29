8299020f894c04dbee4359f2f3570b39ca3c6a10
lilblupig <lilblupig@gmail.com>
2021-05-29 20:10:47 +0100

    Add some text to home page

--- index.php
+++ index.php
@@ -5,11 +5,52 @@
         <meta name="keywords" content="Oblivion, Elder Scrolls, RPG, Open world, Quest tracker">
         <meta name="description" content="Progress tracker for the Bethesda game Oblivion">
         <link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/css/style.css">
         <title>Oblivion Tracker</title>
     </head>
 
     <body>
-    <p>Is dis work</p>
+        <nav>
+        </nav>
+
+        <main>
+            <section class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>About this Oblivion Tracker</h1>
+                    </div>
+                    <div class="col-12">
+                        <p>In Bethesda's Elder Scrolls IV: Oblivion, there are a tremendous number of explorable locations, quests, collectible items and various other information that a player might wish to keep track of.  I personally struggle to keep up with what I have found, and of those, what I have explored.  I have used spreadsheets to keep track of this for years, but, since I play on PS3, it would be nice to have something a bit more phone friendly to hand.</p>
+                        <p>I aim to build trackers for the categories listed out below and shown in the navigation bar, though I suspect this will be slow going as I learn how things work.</p>
+                        <ul>
+                            <li>Weapons</li>
+                            <li>Armour</li>
+                            <li>Settlements</li>
+                            <li>Dungeons</li>
+                            <li>Oblivion Gates</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
+            <section class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p>The base data for the tracker is taken from the fantastic work of the good people at <a href="https://en.uesp.net/wiki/Oblivion:Oblivion" rel="noopener" target="blank">The Unofficial Elder Scrolls Pages</a> Oblivion resource.</p>
+                        <p>The game is made by Bethesda, and all copyrights and intellectual property rights remain theirs.  You can find the <a href="https://elderscrolls.bethesda.net/en/oblivion" rel="noopener" target="blank">Oblivion homepage here</a>.</p>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+
+        <footer>
+        </footer>
+
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    
     </body>
+
 </html>
