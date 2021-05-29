ea44832e0e8ce44d50d9b3d612f16b07794f7946
lilblupig <lilblupig@gmail.com>
2021-05-29 20:53:42 +0100

    Write footer in common document and style

---
+++ footer.php
@@ -0,0 +1,40 @@
+<footer>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 col-md-6 row footer-card">
+        <div class="col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6">
+          <div>
+            Our mission - To help you keep track of your exploration of Tamriel.
+          </div>
+          <div>&nbsp;</div>
+          <div>
+              Design - Lilblupig - May 2021
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 footer-card">
+        Socials
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener">
+              <i class="fab fa-instagram-square px-2 socials"></i>
+              <span class="visually-hidden">Instagram</span>
+              </a>
+          </li>
+          <li class="list-inline-item">
+            <a href="https://www.pinterest.co.uk/" target="_blank" rel="noopener">
+              <i class="fab fa-pinterest-square px-2 socials" aria-hidden="true"></i>
+              <span class="visually-hidden">Pinterest</span>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a href="https://www.youtube.com/" target="_blank" rel="noopener">
+              <i class="fab fa-youtube-square px-2 socials" aria-hidden="true"></i>
+              <span class="visually-hidden">YouTube</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
