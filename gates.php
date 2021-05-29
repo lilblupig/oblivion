cd8b764b3fdbfe11b1e13c133803824f8b956ad0
lilblupig <lilblupig@gmail.com>
2021-05-29 21:37:22 +0100

    Flech out gates page with first few gate buttons

---
+++ gates.php
@@ -0,0 +1,55 @@
+<html lang="en-uk">
+    <?php
+        include "head.php";
+    ?>
+
+    <body>
+        <?php
+            include "nav.php";
+        ?>
+        <main>
+            <section class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>The Oblivion Gates</h1>
+                    </div>
+                    <div class="col-12">
+                        <p>There can be a total of 60 Oblivion Gates which the player can close, though the number of these available at any point in the game varies.</p>
+                        <p>Initially, only one gate is open; the great gate at Kvatch which features in the main quest.  As the player advances through the main quest a number of fixed gates will open, and a varying number of random gates may spawn.</p>
+                        <p>The fixed gates largely relate to main quest required and optional tasks, and the random gates generally spawn when the player is within a certain proximity and certain conditions are met.</p>
+                    </div>
+                </div>
+            </section>
+
+            <section class="container pb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <h3>Appears at the start of the game</h3>
+                        <p><button class="btn tracker-btn">Gate 1</button>The gate in front of Kvatch</p>
+                        <h3>Appear after completing "Find The Heir"</h3>
+                        <p><button class="btn tracker-btn">Gate 14</button>Due east of Leyawiin, beyond Onyx Caverns</p>
+                        <p><button class="btn tracker-btn">Gate 16</button>Southeast of Leyawiin, north of Fort Blueblood</p>
+                        <p><button class="btn tracker-btn">Gate 27</button>A little west of Talwinque</p>
+                        <p><button class="btn tracker-btn">Gate 38</button>Southwest of Kvatch, a little southeast of Garlas Agea</p>
+                        <p><button class="btn tracker-btn">Gate 54</button>A little southeast of Fort Carmala</p>
+                        <p><button class="btn tracker-btn">Gate 55</button>Immediately north of Inn of Ill Omen</p>
+                        <p><button class="btn tracker-btn explored">Gate 65</button>North of Bravil, a little south of Fort Variela</p>
+                        <p><button class="btn tracker-btn found">Gate 69</button>East of Bruma, northwest of Sedor</p>
+                        <p><button class="btn tracker-btn">Gate 93</button>Far northwest of Skingrad, southeast of Talwinque</p>
+                        <p><button class="btn tracker-btn">Gate 99</button>Due north of Skingrad, immediately west of Goblin Jim's Cave</p>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+
+        <?php
+            include "footer.php";
+        ?>
+
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    
+    </body>
+
+</html>
